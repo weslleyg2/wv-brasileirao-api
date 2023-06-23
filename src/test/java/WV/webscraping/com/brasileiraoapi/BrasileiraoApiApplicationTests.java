795e384cdbd0997b58542567cb3fb9fc74b0b992
Weslley Vander <weslleyvander2009@gmail.com>
2023-06-22 22:15:25 -0300

    Uploading initial version of the project

---
+++ src/test/java/WV/webscraping/com/brasileiraoapi/BrasileiraoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package WV.webscraping.com.brasileiraoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
