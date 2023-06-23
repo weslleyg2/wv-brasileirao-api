8eef9f38e820158bc4e69c9ef721c5abdb8c526f
Weslley Vander <weslleyvander2009@gmail.com>
2023-06-23 11:58:51 -0300

    Connecting with Jsoup on google

--- src/main/java/WV/webscraping/com/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/WV/webscraping/com/brasileiraoapi/util/ScrapingUtil.java
@@ -1,18 +1,44 @@
 package WV.webscraping.com.brasileiraoapi.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import WV.webscraping.com.brasileiraoapi.dto.PartidaGoogleDTO;
 
-public class ScrapingUtil {
+	public class ScrapingUtil {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
+	
 	private static final String BASE_URL_GOOGLE = "https://www.google.com/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		PartidaGoogleDTO partida = new PartidaGoogleDTO();
-		partida.getGolEquipeCasa();
 
+	public static void main(String[] args) {
+		String url = BASE_URL_GOOGLE + "palmeiras+x+corinthians+08/08/2020" + COMPLEMENTO_URL_GOOGLE;
+
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obtemInformacoesPartida(url);
+	}
+
+	public PartidaGoogleDTO obtemInformacoesPartida(String url) {
+		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+
+		Document document = null;
+
+		try {
+			document = Jsoup.connect(url).get();
+			
+			String title = document.title();
+			LOGGER.info("Titulo da pagina {}",title);
+			
+		} catch (IOException e) {
+			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
+		}
+
+		return partida;
 	}
 
 }
