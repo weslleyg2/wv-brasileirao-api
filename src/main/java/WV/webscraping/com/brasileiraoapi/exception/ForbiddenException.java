8d56a88570601ab90b9b5a65e086b2684538fc88
Weslley Vander <weslleyvander2009@gmail.com>
2023-06-26 21:22:33 -0300

    Creating a handler to catch exceptions

---
+++ src/main/java/WV/webscraping/com/brasileiraoapi/exception/ForbiddenException.java
@@ -0,0 +1,26 @@
+package wv.webscraping.com.brasileiraoapi.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException{
+	
+	public static final long serialVersionUID = 1L;
+	
+	public ForbiddenException() {
+		super();
+	}
+	
+	public ForbiddenException(String message) {
+		super(message);
+	}
+	
+	public ForbiddenException(Throwable cause) {
+		super(cause);
+	}
+	public ForbiddenException(String message, Throwable cause) {
+		super(message,cause);
+	}
+}
---
+++ src/main/java/WV/webscraping/com/brasileiraoapi/exception/UnauthorizedException.java
@@ -0,0 +1,26 @@
+package wv.webscraping.com.brasileiraoapi.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException{
+	
+	public static final long serialVersionUID = 1L;
+	
+	public UnauthorizedException() {
+		super();
+	}
+	
+	public UnauthorizedException(String message) {
+		super(message);
+	}
+	
+	public UnauthorizedException(Throwable cause) {
+		super(cause);
+	}
+	public UnauthorizedException(String message, Throwable cause) {
+		super(message,cause);
+	}
+}
